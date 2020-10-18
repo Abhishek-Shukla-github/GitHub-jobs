5b44dcd7ea2cd1eb6ae3c8d84ee1069c643b4fe0
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-10-18 14:40:00 +0530

    Refactor Bootstrap form to Material UI Textarea

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Form,Col } from "react-bootstrap";
+import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
 
 
 export default function SearchForm({params,onParamChange}) {
     return (
-        <Form className="mb-4 ">
-            <Form.Row className="align-items-end">
-                <Form.Group as={Col} >
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" onChange={onParamChange} value={params.description} name="description" />
-                </Form.Group>
-                <Form.Group as={Col}>
-                    <Form.Label>Location</Form.Label>
-                    <Form.Control type="text" onChange={onParamChange} value={params.location} name="location" />
-                </Form.Group>
-                <Form.Group className="ml-2" xs="auto">
-                    <Form.Check className="mb-2" onChange={onParamChange} value={params.full_time} id="full-time" name="full-time" label="Only Full Time" type="checkbox"/>
-                </Form.Group>
-            </Form.Row>
-       </Form>
+        <>
+            <form>
+                <TextField style={{margin:"30px"}} label="Positon" type="text" onChange={onParamChange} value={params.description} name="description"/>
+                <TextField style={{margin:"30px"}} label="Location" type="text" onChange={onParamChange} value={params.location} name="location"/>
+                <span style={{margin:"30px"}}> Only Full-time :-
+                    <Checkbox style={{margin:"45px",marginLeft:"0px"}} onChange={onParamChange} value={params.full_time} id="full_time" name="full_time" />
+                </span>
+            </form>
+       </>
     )
 }
