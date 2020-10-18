f92ab2ffd9cbe1ce587104130d10cb2605f7949b
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-10-18 13:14:22 +0530

    Style Navbar using Material UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {Container} from "react-bootstrap";
 import Job from './Job';
 import JobsPagination from "./JobsPagination";
 import SearchForm from './SearchForm';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   const [params, setParams] = useState({})
@@ -22,9 +25,20 @@ function App() {
   return (
     <>
       <div className="App">
-      <Container>
-        <SearchForm params={params} onParamChange={handleParamChange}/>
-        <h1>Github Jobs</h1>
+        <Container>
+          <div style={{backgroundColor:"red",marginBottom:"8rem"}}>
+          <AppBar >
+            <Toolbar>
+              <Typography  variant="h6" color="inherit">
+                GitHub Jobs
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <div className="mt-5">
+          <SearchForm params={params} onParamChange={handleParamChange}/>
+        </div>
+        <h1 className="mb-2">Github Jobs</h1>
           <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
         {loading && <h1>Loading....</h1>}
         {error && <h1>Error :( ...try refreshing</h1>}
